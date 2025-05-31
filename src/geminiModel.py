b09cf6ef790a7ef274219a89897db7462443a0cf
Praveen Kumar <58145557+Praveen163@users.noreply.github.com>
2025-05-31 09:41:21 +0530

    API

--- src/geminiModel.py
+++ src/geminiModel.py
@@ -1,9 +1,9 @@
-
 from dotenv import load_dotenv
 from agents import AsyncOpenAI,OpenAIChatCompletionsModel
+from dotenv import load_dotenv
 import os
 load_dotenv()
-google_api_key = "AIzaSyBMDSTsqb2mKJ57jk5PcDY4Wk1vYN6uZ8s"
+google_api_key = os.getenv("OPENAI_API_KEY") #Google GEMINI API Key
 GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/v1beta/openai/"
 gemini_client = AsyncOpenAI(base_url=GEMINI_BASE_URL, api_key=google_api_key)
 gemini_model = OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=gemini_client)
